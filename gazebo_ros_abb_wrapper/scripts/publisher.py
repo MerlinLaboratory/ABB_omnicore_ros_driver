dcd78cac116252f261e919d3b1fa3b54fb3a0fb7
Andrea Pupa <229867@studenti.unimore.it>
2023-12-15 17:56:36 +0100

    Fized initialization gazebo

---
+++ gazebo_ros_abb_wrapper/scripts/publisher.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+def Publisher():
+    pub = rospy.Publisher('/arm_controller/command', JointTrajectory, queue_size=1)
+    rospy.init_node('init_traj_publisher', anonymous=True)
+    traj = JointTrajectory()
+    names = []
+    for i in range(7):
+        names.append("joint_"+str(i+1))
+    traj.joint_names = names
+    goal = JointTrajectoryPoint()
+    goal.positions = [0]*7
+    goal.time_from_start = rospy.Duration(1)
+    traj.points.append(goal)
+    rospy.sleep(1)
+    pub.publish(traj)
+
+if __name__ == '__main__':
+    try:
+        Publisher()
+    except rospy.ROSInterruptException:
+        pass
